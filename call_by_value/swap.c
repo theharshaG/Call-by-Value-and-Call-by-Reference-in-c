449d5c92745e3099ab561153fb808f59d00f810d
Harsha G <harsha150904@gmail.com>
2026-05-23 21:54:52 +0530

    Implement swap function using call by value

---
+++ call_by_value/swap.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void swap(int,int);
+
+int main()
+{
+    int a=10,b=20;
+
+    printf("Before swap:\n");
+    printf("a=%d b=%d\n",a,b);
+
+    swap(a,b);
+
+    printf("After swap:\n");
+    printf("a=%d b=%d\n",a,b);
+
+    return 0;
+}
+
+void swap(int x,int y)
+{
+    int temp;
+
+    temp=x;
+    x=y;
+    y=temp;
+
+    printf("Inside function:\n");
+    printf("x=%d y=%d\n",x,y);
+}
